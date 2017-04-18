55bd678a701c6289c6db70240b25fbfc482e95eb
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-04-18 09:53:02 +0200

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Spree::Taxonomy.delete_all
-Spree::Taxon.delete_all
+## Taxonomy  category
+## Taxons    artist ouvrage projet shop
 Spree::Product.delete_all
+Spree::Taxon.delete_all
+Spree::Taxonomy.delete_all
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
-## accessory
-artists = Spree::Taxonomy.create!(name: 'Artistes')
+category = Spree::Taxonomy.create!(name: 'Categorie')
 books_taxon = Spree::Taxon.last
-books_category = Spree::Taxon.create!(taxonomy_id: artists.id, parent_id: books_taxon.id, name: 'Ouvrages')
-projects_category = Spree::Taxon.create!(taxonomy_id: artists.id, parent_id:books_taxon.id, name: 'Projets')
+artists_taxon = Spree::Taxon.create!(taxonomy_id: category.id, parent_id: books_taxon.id, name: 'Artistes')
+books_taxon = Spree::Taxon.create!(taxonomy_id: category.id, parent_id: books_taxon.id, name: 'Ouvrages')
+projects_taxon = Spree::Taxon.create!(taxonomy_id: category.id, parent_id:books_taxon.id, name: 'Projets')
+shops_taxon = Spree::Taxon.create!(taxonomy_id: category.id, parent_id:books_taxon.id, name: 'Boutiques')
