2112c0be04d37c610584eea9bfeaf8a9d8a026d8
Quan Trinh <trinhquan1123@yahoo.com>
2017-04-25 12:04:03 +0200

    done with infinite scrolling

--- app/assets/javascripts/product_index.js
+++ app/assets/javascripts/product_index.js
@@ -1,8 +1,14 @@
-$(".panel").hover(
-  function(){
+$(document).on('ready', function() {
+  $(".panel").hover(
+    function(){
       $(".panel-body", this).css("display", "block");
-  }, function(){
+    }, function(){
       $(".panel-body", this).css("display", "none");
-});
+  });
 
-$("img").lazyload();
+  return $("#products").infinitescroll({
+    navSelector: "ul.pagination",
+    nextSelector: "ul.pagination a[rel=next]",
+    itemSelector: "#content .product-list-item"
+  });
+});
