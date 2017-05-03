22cde783d248e29cf2865be77c511d49bcfaa78b
lucien mollard <mollard.lucien@gmail.com>
2017-05-03 15:31:41 +0200

    profile

--- app/helpers/spree/frontend_helper.rb
+++ app/helpers/spree/frontend_helper.rb
@@ -91,5 +91,13 @@ module Spree
         safe_join(taxons, "\n")
       end
     end
+
+        def line_item_description_text description_text
+      if description_text.present?
+        truncate(strip_tags(description_text.gsub('&nbsp;', ' ').squish), length: 100)
+      else
+        Spree.t(:product_has_no_description)
+      end
+    end
   end
 end
