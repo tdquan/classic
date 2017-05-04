c33d16e0a0b918e7d88f5cf6323c1705ce8244a9
lucien mollard <mollard.lucien@gmail.com>
2017-05-04 11:58:12 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,6 @@ namespace :deploy do
       execute "ps -ef | grep classic | grep '[u]nicorn master' | awk '{print $2}' | xargs kill -9"
       execute "export SECRET_KEY_BASE=$(env | grep SECRET_KEY_BASE= | cut -d '='' -f2)"
       execute "/home/classic/.rbenv/shims/unicorn -c ~/staging/current/config/unicorn.rb -E production -D"
-      execute "bundle exec unicorn -c /home/classic/staging/current/config/unicorn.rb -E production -D"
     end
   end
 end
