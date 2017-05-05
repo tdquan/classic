2828f016d1cbe5bece9649962a270a520061cd87
Quan Trinh <trinhquan1123@yahoo.com>
2017-05-05 10:07:31 +0200

    [hot fix] added active tab to navbar

--- app/assets/javascripts/product_index.js
+++ app/assets/javascripts/product_index.js
@@ -1,15 +1,14 @@
 $(document).on('ready', function() {
-//  $(".panel").hover(
-//    function(){
-//      $(".panel-body", this).css("display", "block");
- //   }, function(){
- //     $(".panel-body", this).css("display", "none");
- // });
+  // PRODUCT LIST BOX RESIZE TO SQUARE
   $(".product-list-item img").height($(".product-list-item img").width());
 
   window.onresize = function(event) {
     $(".product-list-item img").height($(".product-list-item img").width());
   };
+
+
+  // ----------------- // -------------------//
+
 
   // INFINITE SCROLLING
   $("#products").infinitescroll({
@@ -17,5 +16,24 @@ $(document).on('ready', function() {
     nextSelector: "ul.pagination a[rel=next]",
     itemSelector: "#content .product-list-item"
   });
+
+  // ----------------- // -------------------//
+
+  // NAVBAR ACTIVE TAB
+  if ($('body').attr('class').includes('taxons show')) {
+    if (window.location.href.includes('/t/artistes')) {
+      $("a.subtaxon-title").removeClass("active");
+      $("a[href='/t/artistes'").addClass("active");
+    } else if (window.location.href.includes('/t/ouvrages')) {
+      $("a.subtaxon-title").removeClass("active");
+      $("a[href='/t/ouvrages'").addClass("active");
+    } else if (window.location.href.includes('/t/projets')) {
+      $("a.subtaxon-title").removeClass("active");
+      $("a[href='/t/projets'").addClass("active");
+    } else if (window.location.href.includes('/t/boutiques')) {
+      $("a.subtaxon-title").removeClass("active");
+      $("a[href='/t/boutiques'").addClass("active");
+    }
+  }
 });
 
