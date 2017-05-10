8363b89a25292e01b6d9fe6bac51a57e098ea799
lucien mollard <mollard.lucien@gmail.com>
2017-05-10 12:06:38 +0200

    order mailer

--- app/mailers/classic_mailer.rb
+++ app/mailers/classic_mailer.rb
@@ -1,8 +1,13 @@
 class ClassicMailer < ActionMailer::Base
   default from: 'contact@classic-paris.com'
 
-    def welcome(user)
-      @user = user
-      mail(to: @user.email, subject: 'Bienvenu sur Classic!')
-    end
+  def welcome(user)
+    @user = user
+    mail(to: @user.email, subject: 'Bienvenu sur Classic!')
+  end
+
+  def new_order(order)
+    @order = order
+    mail(to: @order.email, subject: 'Votre commande sur Classic')
+  end
 end
--- test/mailers/previews/classic_mailer_preview.rb
+++ test/mailers/previews/classic_mailer_preview.rb
@@ -3,4 +3,9 @@ class ClassicMailerPreview < ActionMailer::Preview
     user = Spree::User.first
     ClassicMailer.welcome(user)
   end
+
+  def new_order(order)
+    @order = order
+    mail(to: @order.email, subject: 'Votre commande sur Classic')
+  end
 end
